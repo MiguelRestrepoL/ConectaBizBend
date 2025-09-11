e78e79c897a960633d330b3810683e2ff502fc5e
keivch <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2025-09-10 20:30:02 -0500

    Actualiza el controlador de autenticación para incluir métodos de actualización de perfil y cambio de contraseña, y agrega nuevas rutas para estas funcionalidades. Se mejora la configuración de los modelos para soportar nuevos campos en el modelo de usuario y se ajustan las importaciones a ES6 en varios archivos.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { register, login, acceptUserTerms, logout } from '../controllers/auth.controller.js';
+import { register, login, acceptUserTerms, logout, updateUserProfile, changePassword, getUserById } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.post('/auth/logout', logout);
+router.get('/auth/:id', getUserById);
 router.post('/auth/:id/accept-terms', acceptUserTerms);
+router.put('/auth/:id/profile', updateUserProfile);
+router.put('/auth/:id/change-password', changePassword);
 
 export default router;
 
